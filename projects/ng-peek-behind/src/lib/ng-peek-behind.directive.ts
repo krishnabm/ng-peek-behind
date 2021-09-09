3d293a8059e68d0874a12da16ac2adc6dcfc0462
Krish <krmahade@microsoft.com>
2021-09-09 14:24:52 +0530

    moved logic to hostListeners

--- projects/ng-peek-behind/src/lib/ng-peek-behind.directive.ts
+++ projects/ng-peek-behind/src/lib/ng-peek-behind.directive.ts
@@ -10,14 +10,12 @@ import {
   ElementRef,
   HostListener,
   Input,
-  OnChanges,
-  SimpleChanges,
 } from '@angular/core';
 
 @Directive({
   selector: '[libPeekBehind]',
 })
-export class NgPeekBehindDirective implements OnChanges {
+export class NgPeekBehindDirective {
   //#region properties
   private element: HTMLElement;
   private panelParentElement: HTMLElement;
@@ -35,67 +33,41 @@ export class NgPeekBehindDirective implements OnChanges {
       '.' + (this.peekPanelClass || 'cdk-overlay-pane')
     ) as HTMLElement;
   }
-
-  @Input()
-  public libPeekBehindTrigger: boolean;
   //#endregion
 
   constructor(private host: ElementRef, private builder: AnimationBuilder) {}
 
   //#region Lifecycle hooks
-  ngOnChanges(changes: SimpleChanges) {
-    if (
-      changes &&
-      changes.libPeekBehindTrigger &&
-      !changes.libPeekBehindTrigger.isFirstChange()
-    ) {
-      const show = changes.libPeekBehindTrigger.currentValue;
-
-      if (this.player) {
-        this.player.destroy();
-      }
-
-      const metadata = show ? this.fadeIn() : this.fadeOut();
-      const factory = this.builder.build(metadata);
-
-      if (this.panelParentElement) {
-        this.player = factory.create(this.panelParentElement);
-        this.player.play();
-      } else {
-        throw new Error("NgPeekBehindDirective: Couldn't find parent panel");
-      }
-    }
-  }
   //#endregion
 
   //#region Listeners
-  // @HostListener('document:mouseup', ['$event']) showPanel(_) {
-  //   if (this.host) {
-  //     this.element = this.host.nativeElement;
-  //     this.panelParentElement = this.element.closest(
-  //       '.' + (this.peekPanelClass || 'cdk-overlay-pane')
-  //     ) as HTMLElement;
-  //     const animation = this.panelParentElement.animate([{ opacity: '100%' }], {
-  //       duration: 400,
-  //       fill: 'forwards',
-  //     });
-  //     animation.play();
-  //   }
-  // }
+  @HostListener('document:mouseup', ['$event']) showPanel(_) {
+    if (this.host) {
+      this.element = this.host.nativeElement;
+      this.panelParentElement = this.element.closest(
+        '.' + (this.peekPanelClass || 'cdk-overlay-pane')
+      ) as HTMLElement;
+      const animation = this.panelParentElement.animate([{ opacity: '100%' }], {
+        duration: 400,
+        fill: 'forwards',
+      });
+      animation.play();
+    }
+  }
 
-  // @HostListener('mousedown', ['$event']) hidePanel(_) {
-  //   if (this.host) {
-  //     this.element = this.host.nativeElement;
-  //     this.panelParentElement = this.element.closest(
-  //       '.' + (this.peekPanelClass || 'cdk-overlay-pane')
-  //     ) as HTMLElement;
-  //     const animation = this.panelParentElement.animate([{ opacity: '0%' }], {
-  //       duration: 400,
-  //       fill: 'forwards',
-  //     });
-  //     animation.play();
-  //   }
-  // }
+  @HostListener('mousedown', ['$event']) hidePanel(_) {
+    if (this.host) {
+      this.element = this.host.nativeElement;
+      this.panelParentElement = this.element.closest(
+        '.' + (this.peekPanelClass || 'cdk-overlay-pane')
+      ) as HTMLElement;
+      const animation = this.panelParentElement.animate([{ opacity: '0%' }], {
+        duration: 400,
+        fill: 'forwards',
+      });
+      animation.play();
+    }
+  }
   //#endregion
 
   //#region methods
